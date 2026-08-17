b8ec5d67a1e4cd662716e24fb2513bbc063293b9
PierreViland <pierre.viland@ac-rennes.fr>
2026-08-17 11:34:40 +0200

    modification via terminal

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -25,7 +25,7 @@ public class HelloWorld {
      * @param args Arguments de la ligne de commande. 
      */
     public static void main(String[] args) {
-        System.out.println("Hello World by PV on github!");
+        System.out.println("Hello World by PV on github via terminal!");
         
     }
     
